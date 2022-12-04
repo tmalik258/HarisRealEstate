e031b7b768376c62f92d0b782951f199a0d8aee8
Talha Shahid <talhamalik25.tm@gmail.com>
2022-12-04 14:46:01 +0500

    Added static files and added models

--- HarisRealEstate/settings.py
+++ HarisRealEstate/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -81,6 +81,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'website.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -112,11 +113,16 @@ USE_I18N = True
 
 USE_TZ = True
 
+DATETIME_FORMAT="%Y-%m-%d%H:%M:%S"
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- website/models.py
+++ website/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 # Create your models here.
+
+class User (AbstractUser):
+	pass
+
+class Category (models.Model):
+	code = models.CharField(primary_key=True, max_length=24)
+	category = models.CharField(max_length=24)
+	def __str__(self):
+		return f"{self.category}"
+
+class listing (models.Model):
+	creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="listingUser")
+	title = models.CharField(max_length=64)
+	price = models.DecimalField(max_digits=15, decimal_places=4)
+	category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="cat")
+	address = models.TextField(max_length=100)
+	description = models.TextField(max_length=255)
+	time_created = models.DateTimeField(auto_created=True)
+	active = models.BooleanField(default=True)
+
+	def __str__(self) -> str:
+		if self.active:
+			listing = 'Listing is Active'
+		else:
+			listing = 'Listing is Closed'
+		time = self.time_created.strftime("%H:%M | %d-%m-%Y")
+		return f"Listing: {self.id} | {self.title} | ({self.creator}, {time}) | {listing}"
+
+class Comments (models.Model):
+	fname = models.CharField(max_length=24)
+	lname = models.CharField(max_length=24)
+	listing = models.ForeignKey(listing, on_delete=models.CASCADE, related_name="comments")
+	comment = models.TextField(max_length=255)
+	date_created = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		time = self.date_created.strftime("%H:%M | %d-%m-%Y")
+		return f"{self.creator} | {self.auction_list.title} | {time}"
+
+class Images (models.Model):
+	def user_directory_path(instance, filename):
+        # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+		return 'images/user_{0}/{1}'.format(instance.creator.username, filename)
+	images = models.ImageField(upload_to = user_directory_path, blank=True)
+	listing = models.ForeignKey(listing, on_delete= models.CASCADE, related_name='img')
