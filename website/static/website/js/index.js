967042a06f6e0c2c3b6ff2d9ce068aed1f872dc4
Talha Shahid <talhamalik25.tm@gmail.com>
2022-12-26 18:19:13 +0500

    Fixed some errors

--- website/static/website/js/index.js
+++ website/static/website/js/index.js
@@ -1,6 +1,7 @@
 $(window).on("load",function(){
 	setTimeout(() => {
 		$('.loader').css('transform', 'translateY(-100%)');
+		$('header').css('display', 'initial');
 		$('main').css('display', 'block');
 	}, 700);
 });
