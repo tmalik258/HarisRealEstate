4747a9d3404089d4e926ed19d5bb7f3001f65653
Talha Shahid <talhamalik25.tm@gmail.com>
2022-12-28 05:16:22 +0500

    Added carousel

--- website/static/website/js/index.js
+++ website/static/website/js/index.js
@@ -4,5 +4,36 @@ $(window).on("load",function(){
 		$('header').css('display', 'initial');
 		$('main').css('display', 'block');
 	}, 700);
-
+	
+	if (window.innerWidth > 990)
+	{
+		$('.carousel-inner').height(window.innerHeight);
+		$('.top-categories').height(window.innerHeight);
+	}
 });
+
+window.onscroll = function () {
+	if (window.scrollY >= 50)
+	{
+		$('.navbar').addClass('active');
+		hideHeader();
+	}
+	else
+	{
+		$('.navbar').removeClass('active');
+	}
+}
+
+let lastScrollPos = 0;
+
+const hideHeader = function () {
+	const isScrollButton = lastScrollPos < window.scrollY;
+	if (isScrollButton){
+		$('.navbar').addClass('hide');
+		// $('.navbar').width('100%');
+	}
+	else
+		$('.navbar').removeClass('hide');
+
+	lastScrollPos = window.scrollY;
+}
