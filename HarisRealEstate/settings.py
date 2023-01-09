16cf54b4ca3b9593da7c516fa192ac347d341996
Talha Shahid <talhamalik25.tm@gmail.com>
2023-01-10 03:02:22 +0500

    updated allowed hosts

--- HarisRealEstate/settings.py
+++ HarisRealEstate/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://web-production-6874.up.railway.app/']
+ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = ['https://web-production-6874.up.railway.app/']
 
