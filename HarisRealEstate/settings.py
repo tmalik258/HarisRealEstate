4fb30d7dfb007c5ac3aecc5674c9affaaca21604
Talha Shahid <talhamalik25.tm@gmail.com>
2023-01-09 23:41:12 +0500

    Config for railway deployment part 2

--- HarisRealEstate/settings.py
+++ HarisRealEstate/settings.py
@@ -11,16 +11,19 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Load .env file for variables and key
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5*%%n!1#x0#_06vj(9u&!#xg@auw6*b)(qdggj3v@i389z3(7c'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
