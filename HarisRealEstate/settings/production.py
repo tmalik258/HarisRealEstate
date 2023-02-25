19358c2230a05ab59e286e9c5f1315a1b84a8286
Talha Malik <talhamalik25.tm@gmail.com>
2023-02-25 06:26:26 +0000

    Updated layout.html and debug=True

--- HarisRealEstate/settings/production.py
+++ HarisRealEstate/settings/production.py
@@ -15,8 +15,8 @@ CSRF_TRUSTED_ORIGINS = ['https://harisrealestate.com', 'https://www.harisrealest
 SESSION_COOKIE_SECURE = True
 
 ## HTTPS
-#SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = True
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SECURE_SSL_REDIRECT = True
 
 
 DATABASE_URL = os.getenv("DATABASE_URL")
