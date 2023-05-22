4e7a6288175dd08e983a593655d6ab097c77a71c
Talha Malik <talhamalik25.tm@gmail.com>
2023-05-22 17:48:54 +0500

    Added staticfiles to aws s3

--- HarisRealEstate/settings/base.py
+++ HarisRealEstate/settings/base.py
@@ -112,7 +112,7 @@ DATETIME_FORMAT="%Y-%m-%d%H:%M:%S"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'website/static')
 ]
--- HarisRealEstate/settings/local.py
+++ HarisRealEstate/settings/local.py
@@ -13,3 +13,5 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+STATIC_URL = '/static/'
