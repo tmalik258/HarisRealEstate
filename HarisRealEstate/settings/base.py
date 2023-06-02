fdf51bc1c4b93535fb602773b0ea78472285155a
Talha Malik <talhamalik25.tm@gmail.com>
2023-06-02 15:34:52 +0500

    Removed template directory

--- HarisRealEstate/settings/base.py
+++ HarisRealEstate/settings/base.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'HarisRealEstate.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['website\\templates\\'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
