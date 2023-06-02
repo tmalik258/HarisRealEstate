a0b0dbd899de9286219867eec43fb774a8771ea0
Talha Malik <talhamalik25.tm@gmail.com>
2023-06-02 16:14:05 +0500

    Uncommenting auth url

--- HarisRealEstate/urls.py
+++ HarisRealEstate/urls.py
@@ -25,7 +25,7 @@ admin.site.empty_value_display = '(Empty)'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('accounts/', include('allauth.urls')),
+    path('accounts/', include('allauth.urls')),
     path('', include('website.urls')),
 ]
 
