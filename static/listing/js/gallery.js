34a16bfb6b5b19706689cfa50f94d7289134d6d1
Talha Malik <talhamalik25.tm@gmail.com>
2023-07-17 02:47:15 +0500

    Added responsiveness to open gallery button

--- static/listing/js/gallery.js
+++ static/listing/js/gallery.js
@@ -1,9 +1,11 @@
-$('.image-gallery .close_btn').click(function (e) { 
-	e.preventDefault();
-	$('.image-gallery').css('display', 'none');
-});
+if (window.innerWidth > 990) {
+	$('.image-gallery .close_btn').click(function (e) { 
+		e.preventDefault();
+		$('.image-gallery').css('display', 'none');
+	});
 
-$('.open_gallery').click(function (e) { 
-	e.preventDefault();
-	$('.image-gallery').css('display', 'flex');
-});
+	$('.open_gallery').click(function (e) { 
+		e.preventDefault();
+		$('.image-gallery').css('display', 'flex');
+	});
+}
