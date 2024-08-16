8bbbabe1a6f0bb7a6eece9f6ae23344c1ff102d5
tmalik258 <talhamalik25.tm@gmail.com>
2024-08-16 12:53:53 +0500

    Fixed media

--- core/urls.py
+++ core/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     path('', include('listing.urls', namespace='listing')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
