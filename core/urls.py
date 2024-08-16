86b8a2ed78cbedbca2b01dceb4c2be89ffcec7b0
tmalik258 <talhamalik25.tm@gmail.com>
2024-08-16 16:47:36 +0500

    Updated urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 admin.site.site_header = 'Haris Real Estate Administration'
 admin.site.site_title = 'Site Admin'
@@ -14,5 +15,7 @@ urlpatterns = [
     path('', include('listing.urls', namespace='listing')),
 ]
 
-# if settings.DEBUG:
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += re_path(r'^media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
