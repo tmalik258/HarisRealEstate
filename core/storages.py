c29d4cda65f30eff33a1d6f6ee9bd1c7ef318e49
tmalik258 <talhamalik25.tm@gmail.com>
2024-08-21 16:22:19 +0500

    Updated storages.py

--- core/storages.py
+++ core/storages.py
@@ -3,7 +3,3 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class MediaStore (S3Boto3Storage):
 	location = 'media'
 	fileoverwrite = True
-
-	def __init__(self, *args, **kwargs):
-		kwargs['custom_domain'] = False
-		super().__init__(*args, **kwargs)
