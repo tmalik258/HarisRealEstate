43b7d6ebd32ad63371deebf68d2c05148ca7a55b
tmalik258 <talhamalik25.tm@gmail.com>
2024-08-29 13:20:37 +0500

    Updated StaticManifestS3Storage to fix no storage issues

--- core/storages.py
+++ core/storages.py
@@ -1,37 +1,21 @@
-from typing import Any
 from storages.backends.s3boto3 import S3Boto3Storage
 from django.contrib.staticfiles.storage import ManifestStaticFilesStorage
-from django.core.files.storage import FileSystemStorage
 
-class MediaStore (S3Boto3Storage):
-	location = 'media'
-	fileoverwrite = True
+class MediaStore(S3Boto3Storage):
+    location = 'media'
+    file_overwrite = True
 
+class StaticManifestS3Storage(ManifestStaticFilesStorage, S3Boto3Storage):
+    location = 'static'
 
-class StaticManifestS3Storage(ManifestStaticFilesStorage):
-	location = 'static'
+    def __init__(self, *args, **kwargs):
+        # Remove 'storage' from kwargs if present
+        kwargs.pop('storage', None)
+        S3Boto3Storage.__init__(self, *args, **kwargs)
+        ManifestStaticFilesStorage.__init__(self, *args, **kwargs)
 
-	def __init__(self, *args, **kwargs):
-		kwargs['storage'] = S3Boto3Storage()
-		super().__init__(*args, **kwargs)
-		self.local_storage = FileSystemStorage()
-	
-	def path(self, name):
-		return self.local_storage.path(name)
+    def hashed_name(self, name, content=None, filename=None):
+        return ManifestStaticFilesStorage.hashed_name(self, name, content, filename)
 
-	def hashed_name(self, name, content=None, filename=None):
-		# Implement hashed_name to use ManifestStaticFilesStorage's hashing
-		return super().hashed_name(name, content, filename)
-
-	def url(self, name, parameters=None, expire=None):
-		# Use S3Boto3Storage's url method
-		return self.storage.url(self._stored_name(name), parameters=parameters, expire=expire)
-
-	def _store_name(self, name):
-		return self.stored_name.get(name, name)
-	
-	def read_manifest(self) -> Any:
-		try:
-			return super().read_manifest()
-		except FileNotFoundError:
-			return None
+    def url(self, name, parameters=None, expire=None):
+        return S3Boto3Storage.url(self, name, parameters=parameters, expire=expire)
